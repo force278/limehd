55d62826a5f2227a71b0c6e7a385d113ef33a29f
iVanXX <nikandrov.van@mail.ru>
2023-12-08 18:04:51 +0300

    main

--- backend/drflimehd/drflimehd/urls.py
+++ backend/drflimehd/drflimehd/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from limehd.views import ChannelAPIList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/playlist', ChannelAPIList.as_view()),
 ]
