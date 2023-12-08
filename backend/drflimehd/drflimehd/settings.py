a6c05e14cf88a196aac45399ace47c17ff51b0cc
force278 <60031519+force278@users.noreply.github.com>
2023-12-08 17:59:56 +0300

    fix

--- backend/drflimehd/drflimehd/settings.py
+++ backend/drflimehd/drflimehd/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'limehd.apps.LimehdConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- backend/drflimehd/drflimehd/urls.py
+++ backend/drflimehd/drflimehd/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from limehd.views import ChannelAPIList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('channels/', ChannelAPIList.as_view())
 ]
